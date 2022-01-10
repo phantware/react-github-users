87bc85c86225b788dea3435d25a7956496c70e25
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-10 09:22:26 +0100

    Login  auth implemented

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import App from './App'
 import { GithubProvider } from './context/context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+console.log('DOMAIN', process.env.REACT_APP_DOMAIN)
+
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN}
+      clientId={process.env.REACT_APP_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
